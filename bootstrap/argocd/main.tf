e1c9c14ebf0120f47726e176607f7744f56aecec
Arya Tavakoli <25291801+aryatavakoli@users.noreply.github.com>
2024-09-09 04:35:21 -0700

    Build out ArgoCD Cluster

--- bootstrap/argocd/main.tf
+++ bootstrap/argocd/main.tf
@@ -10,3 +10,21 @@ resource "helm_release" "argocd" {
     file("${path.module}/values.yaml")
   ]
 }
+
+resource "kubernetes_manifest" "ArgoCdBootStrapProject" {
+  manifest = {
+    apiVersion = "argoproj.io/v1alpha1"
+    kind       = "AppProject"
+    metadata = {
+      name      = "project-bootstrap"
+      namespace = "argocd"
+      finalizers = [
+        "resources-finalizer.argocd.argoproj.io"
+      ]
+    }
+    spec = {
+      description = "Homelab Bootstrap Charts"
+      sourceRepos = ["*"]
+    }
+  }
+}
