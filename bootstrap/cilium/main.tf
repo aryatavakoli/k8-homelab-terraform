933698d2469584b6e3a64dddbb4b07e2f2e2568f
Arya Tavakoli <25291801+aryatavakoli@users.noreply.github.com>
2024-09-11 21:25:28 -0700

    Change Cilium Namespace

--- bootstrap/cilium/main.tf
+++ bootstrap/cilium/main.tf
@@ -3,7 +3,7 @@ resource "helm_release" "cilium" {
   repository       = "https://helm.cilium.io/"
   chart            = "cilium"
   version          = "1.16.1"
-  namespace        = "cilium"
+  namespace        = "kube-system"
   create_namespace = "true"
 
   values = [
