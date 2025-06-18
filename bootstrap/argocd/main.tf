5bdd3f37222f75460d707c26ed6c707fcdede5ca
Arya Tavakoli <25291801+aryatavakoli@users.noreply.github.com>
2025-06-18 00:36:01 -0700

    Add Services to ArgoCD

--- bootstrap/argocd/main.tf
+++ bootstrap/argocd/main.tf
@@ -74,3 +74,67 @@ resource "kubernetes_manifest" "ArgoCdHomeLabBootstrapApplication" {
     }
   }
 }
+
+resource "kubernetes_manifest" "ArgoCdServicesProject" {
+  depends_on = [kubernetes_manifest.ArgoCdHomeLabBootstrapApplication]
+  manifest = {
+    apiVersion = "argoproj.io/v1alpha1"
+    kind       = "AppProject"
+    metadata = {
+      name      = "project-services"
+      namespace = "argocd"
+      finalizers = [
+        "resources-finalizer.argocd.argoproj.io"
+      ]
+    }
+    spec = {
+      description = "Homelab Services Charts"
+      sourceRepos = ["*"]
+      destinations = [{
+        namespace = "*"
+        server    = "*"
+      }]
+      clusterResourceWhitelist = [{
+        group = "*"
+        kind  = "*"
+      }]
+    }
+  }
+}
+
+resource "kubernetes_manifest" "ArgoCdHomeLabServicesApplication" {
+  depends_on = [kubernetes_manifest.ArgoCdHomeLabBootstrapApplication]
+  manifest = {
+    apiVersion = "argoproj.io/v1alpha1"
+    kind       = "Application"
+    metadata = {
+      name      = "homelab-services"
+      namespace = "argocd"
+      finalizers = [
+        "resources-finalizer.argocd.argoproj.io"
+      ]
+    }
+    spec = {
+      project = "project-services"
+      destination = {
+        namespace = "argocd"
+        name      = "in-cluster"
+      }
+      source = {
+        path           = "argocd/apps/services"
+        repoURL        = "https://github.com/aryatavakoli/k8-homelab-charts"
+        targetRevision = "HEAD"
+      }
+      syncPolicy = {
+        automated = {
+          allowEmpty = true
+          prune      = true
+          selfHeal   = true
+        }
+        syncOptions = [
+          "CreateNamespace=true"
+        ]
+      }
+    }
+  }
+}
