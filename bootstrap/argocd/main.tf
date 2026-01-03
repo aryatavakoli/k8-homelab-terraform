27ca9a5a0714a99d345fbcd23855408903c8093a
Arya Tavakoli <25291801+aryatavakoli@users.noreply.github.com>
2026-01-02 20:52:43 -0800

    Update Cilium and Argocd

--- bootstrap/argocd/main.tf
+++ bootstrap/argocd/main.tf
@@ -2,7 +2,7 @@ resource "helm_release" "argocd" {
   name             = "argo-cd"
   repository       = "https://argoproj.github.io/argo-helm"
   chart            = "argo-cd"
-  version          = "9.1.1"
+  version          = "9.2.4"
   namespace        = "argocd"
   create_namespace = "true"
 
